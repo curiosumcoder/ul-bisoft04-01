17135813ebb32947a82ca6655c1c57af98c171ae
Gilberto Bermúdez Garro <gbermude@outlook.com>
2023-05-23 21:52:12 -0600

    Update

---
+++ 20230523/wa1/wa1/Form1.Designer.vb
@@ -0,0 +1,194 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.NorthwindDataSet = New wa1.NorthwindDataSet()
+        Me.CustomersBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.CustomersTableAdapter = New wa1.NorthwindDataSetTableAdapters.CustomersTableAdapter()
+        Me.CustomerIDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CompanyNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ContactNameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ContactTitleDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.AddressDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CityDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.RegionDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PostalCodeDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CountryDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PhoneDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.FaxDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.bSave = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NorthwindDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.CustomersBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToOrderColumns = True
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.CustomerIDDataGridViewTextBoxColumn, Me.CompanyNameDataGridViewTextBoxColumn, Me.ContactNameDataGridViewTextBoxColumn, Me.ContactTitleDataGridViewTextBoxColumn, Me.AddressDataGridViewTextBoxColumn, Me.CityDataGridViewTextBoxColumn, Me.RegionDataGridViewTextBoxColumn, Me.PostalCodeDataGridViewTextBoxColumn, Me.CountryDataGridViewTextBoxColumn, Me.PhoneDataGridViewTextBoxColumn, Me.FaxDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.CustomersBindingSource
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(0, 0)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(800, 368)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'NorthwindDataSet
+        '
+        Me.NorthwindDataSet.DataSetName = "NorthwindDataSet"
+        Me.NorthwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'CustomersBindingSource
+        '
+        Me.CustomersBindingSource.DataMember = "Customers"
+        Me.CustomersBindingSource.DataSource = Me.NorthwindDataSet
+        '
+        'CustomersTableAdapter
+        '
+        Me.CustomersTableAdapter.ClearBeforeFill = True
+        '
+        'CustomerIDDataGridViewTextBoxColumn
+        '
+        Me.CustomerIDDataGridViewTextBoxColumn.DataPropertyName = "CustomerID"
+        Me.CustomerIDDataGridViewTextBoxColumn.HeaderText = "CustomerID"
+        Me.CustomerIDDataGridViewTextBoxColumn.Name = "CustomerIDDataGridViewTextBoxColumn"
+        '
+        'CompanyNameDataGridViewTextBoxColumn
+        '
+        Me.CompanyNameDataGridViewTextBoxColumn.DataPropertyName = "CompanyName"
+        Me.CompanyNameDataGridViewTextBoxColumn.HeaderText = "CompanyName"
+        Me.CompanyNameDataGridViewTextBoxColumn.Name = "CompanyNameDataGridViewTextBoxColumn"
+        '
+        'ContactNameDataGridViewTextBoxColumn
+        '
+        Me.ContactNameDataGridViewTextBoxColumn.DataPropertyName = "ContactName"
+        Me.ContactNameDataGridViewTextBoxColumn.HeaderText = "ContactName"
+        Me.ContactNameDataGridViewTextBoxColumn.Name = "ContactNameDataGridViewTextBoxColumn"
+        '
+        'ContactTitleDataGridViewTextBoxColumn
+        '
+        Me.ContactTitleDataGridViewTextBoxColumn.DataPropertyName = "ContactTitle"
+        Me.ContactTitleDataGridViewTextBoxColumn.HeaderText = "ContactTitle"
+        Me.ContactTitleDataGridViewTextBoxColumn.Name = "ContactTitleDataGridViewTextBoxColumn"
+        '
+        'AddressDataGridViewTextBoxColumn
+        '
+        Me.AddressDataGridViewTextBoxColumn.DataPropertyName = "Address"
+        Me.AddressDataGridViewTextBoxColumn.HeaderText = "Address"
+        Me.AddressDataGridViewTextBoxColumn.Name = "AddressDataGridViewTextBoxColumn"
+        '
+        'CityDataGridViewTextBoxColumn
+        '
+        Me.CityDataGridViewTextBoxColumn.DataPropertyName = "City"
+        Me.CityDataGridViewTextBoxColumn.HeaderText = "City"
+        Me.CityDataGridViewTextBoxColumn.Name = "CityDataGridViewTextBoxColumn"
+        '
+        'RegionDataGridViewTextBoxColumn
+        '
+        Me.RegionDataGridViewTextBoxColumn.DataPropertyName = "Region"
+        Me.RegionDataGridViewTextBoxColumn.HeaderText = "Region"
+        Me.RegionDataGridViewTextBoxColumn.Name = "RegionDataGridViewTextBoxColumn"
+        '
+        'PostalCodeDataGridViewTextBoxColumn
+        '
+        Me.PostalCodeDataGridViewTextBoxColumn.DataPropertyName = "PostalCode"
+        Me.PostalCodeDataGridViewTextBoxColumn.HeaderText = "PostalCode"
+        Me.PostalCodeDataGridViewTextBoxColumn.Name = "PostalCodeDataGridViewTextBoxColumn"
+        '
+        'CountryDataGridViewTextBoxColumn
+        '
+        Me.CountryDataGridViewTextBoxColumn.DataPropertyName = "Country"
+        Me.CountryDataGridViewTextBoxColumn.HeaderText = "Country"
+        Me.CountryDataGridViewTextBoxColumn.Name = "CountryDataGridViewTextBoxColumn"
+        '
+        'PhoneDataGridViewTextBoxColumn
+        '
+        Me.PhoneDataGridViewTextBoxColumn.DataPropertyName = "Phone"
+        Me.PhoneDataGridViewTextBoxColumn.HeaderText = "Phone"
+        Me.PhoneDataGridViewTextBoxColumn.Name = "PhoneDataGridViewTextBoxColumn"
+        '
+        'FaxDataGridViewTextBoxColumn
+        '
+        Me.FaxDataGridViewTextBoxColumn.DataPropertyName = "Fax"
+        Me.FaxDataGridViewTextBoxColumn.HeaderText = "Fax"
+        Me.FaxDataGridViewTextBoxColumn.Name = "FaxDataGridViewTextBoxColumn"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.DataGridView1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(800, 368)
+        Me.Panel1.TabIndex = 1
+        '
+        'bSave
+        '
+        Me.bSave.Location = New System.Drawing.Point(713, 374)
+        Me.bSave.Name = "bSave"
+        Me.bSave.Size = New System.Drawing.Size(75, 23)
+        Me.bSave.TabIndex = 2
+        Me.bSave.Text = "S&ave"
+        Me.bSave.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.bSave)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NorthwindDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.CustomersBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents NorthwindDataSet As NorthwindDataSet
+    Friend WithEvents CustomersBindingSource As BindingSource
+    Friend WithEvents CustomersTableAdapter As NorthwindDataSetTableAdapters.CustomersTableAdapter
+    Friend WithEvents CustomerIDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CompanyNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ContactNameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ContactTitleDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents AddressDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CityDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents RegionDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents PostalCodeDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CountryDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents PhoneDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents FaxDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents bSave As Button
+End Class
---
+++ 20230523/wa1/wa1/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: esta línea de código carga datos en la tabla 'NorthwindDataSet.Customers' Puede moverla o quitarla según sea necesario.
+        Me.CustomersTableAdapter.Fill(Me.NorthwindDataSet.Customers)
+
+    End Sub
+
+    Private Sub bSave_Click(sender As Object, e As EventArgs) Handles bSave.Click
+
+        ' Me.CustomersTableAdapter.Update(Me.NorthwindDataSet.Customers)
+
+        Dim hoyEsMartes As Boolean
+        hoyEsMartes = True
+        hoyEsMartes = False
+
+        Dim noHayCambios As Boolean = Me.NorthwindDataSet.Customers.GetChanges() Is Nothing
+        Dim hayCambios As Boolean = Not noHayCambios
+
+        If Not Me.NorthwindDataSet.Customers.GetChanges() Is Nothing Then
+            Me.CustomersTableAdapter.Update(Me.NorthwindDataSet.Customers)
+            MessageBox.Show("Changes have been applied!",
+                            "Information", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+        Else
+            MessageBox.Show("No changes to be applied!",
+                            "Information", MessageBoxButtons.OK,
+                            MessageBoxIcon.Warning)
+        End If
+    End Sub
+End Class
